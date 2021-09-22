ca21e338f86dedd04558b5afb07dcd2938a0ef70
Joaquin Gonzalez <mausaguer@outlook.com>
2021-09-22 17:49:03 -0300

    added script to mask password

--- nifi_TA_monitoring/bin/nifi.py
+++ nifi_TA_monitoring/bin/nifi.py
@@ -4,15 +4,20 @@ import json
 import requests
 import urllib3
 import dotenv
-from splunklib.modularinput import *
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
+import splunklib.client as client
+from splunklib.modularinput import *
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 
 class NiFiScript(Script):
+    
 
+    # Define some global variables
+    MASK           = "********"
 
     def get_scheme(self):
         scheme = Scheme("NiFi")
@@ -29,7 +34,7 @@ class NiFiScript(Script):
             required_on_create=True
         )
         scheme.add_argument(name_argument)
-        
+
         url_argument = Argument(
             name="api_url",
             description="NiFi instance API URL",
@@ -39,7 +44,7 @@ class NiFiScript(Script):
             required_on_create=True
         )
         scheme.add_argument(url_argument)
-
+        
         auth_type_argument = Argument(
             name="auth_type",
             description="Auth Type",
@@ -69,7 +74,7 @@ class NiFiScript(Script):
             required_on_create=False
         )
         scheme.add_argument(password_argument)
-        
+
         return scheme
 
 
@@ -98,7 +103,10 @@ class NiFiScript(Script):
             EventWriter.log(ew, EventWriter.ERROR, 'Error token request - {}'.format(error))
 
 
-    def get_request(self, ew, base_url, path, auth_type, username, password, input_name):
+    def get_request(self, ew, base_url, path, auth_type, username, input_name, session_key):
+        
+        password = self.get_password(session_key, username)
+
         EventWriter.log(ew, EventWriter.INFO, 'Resquest base_url:{} path:{}, auth_type:{}, input_name:{}'.format(base_url, path, auth_type, input_name))
         if auth_type == "none":
             headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
@@ -138,40 +146,118 @@ class NiFiScript(Script):
             except Exception as error:
                 EventWriter.log(ew, EventWriter.ERROR, 'Error request - {}'.format(error))
 
+
+    # New code
+    def encrypt_password(self, username, password, session_key):
+        args = {'token': session_key}
+        service = client.connect(**args)
+      
+        try:
+            for storage_password in service.storage_passwords:
+                if storage_password.username == username:
+                    service.storage_passwords.delete(username=storage_password.username)
+                    break
+ 
+            service.storage_passwords.create(password, username)
+ 
+        except Exception as e:
+            raise Exception("An error occurred updating credentials. Please ensure your user account has admin_all_objects and/or list_storage_passwords capabilities. Details: %s" % str(e))
+
+
+    def mask_password(self, session_key, username, base_url, auth_type, input_name):
+        try:
+            args = {'token':session_key}
+            service = client.connect(**args)
+            kind, input_name = input_name.split("://")
+            item = service.inputs.__getitem__((input_name, kind))
+           
+            kwargs = {
+                "username": username,
+                "api_url": base_url,
+                "auth_type": auth_type,
+                "password": self.MASK
+            }
+            item.update(**kwargs).refresh()
+           
+        except Exception as e:
+            raise Exception("Error updating inputs.conf: %s" % str(e))
+            
     
+    def get_password(self, session_key, username):
+        args = {'token':session_key}
+        service = client.connect(**args)
+        
+        # Retrieve the password from the storage/passwords endpoint	
+        for storage_password in service.storage_passwords:
+            if storage_password.username == username:
+                return storage_password.content.clear_password
+
+# End new code
 
     def stream_events(self, inputs, ew):
+
+        ew.log('INFO', '########## STREAMING EVENTS ##########')
+
         endpoints = [
             {"sourcetype":"nifi:api:flow_status", "path":"/flow/status"},
             {"sourcetype":"nifi:api:system_diagnostics", "path":"/system-diagnostics"},
             {"sourcetype":"nifi:api:site_to_site", "path":"/site-to-site"},
-            {"sourcetype":"nifi:api:controller_cluster", "path":"/controller/cluster"},
         ]
 
-        #for input_name, input_item in inputs.inputs.iteritems():
         input_name, input_item = inputs.inputs.popitem()
+        session_key = self._input_definition.metadata['session_key']
         base_url = input_item.get("api_url")
         auth_type = input_item.get("auth_type")
         username = input_item.get("username", None)
         password = input_item.get("password", None)
 
+        ew.log('INFO','auth type: ' + auth_type)
+
         kind, iname = input_name.split("://")
 
         EventWriter.log(ew, EventWriter.INFO, "Started Nifi Get Data for input: {}".format(input_name))
 
-        for ep in endpoints:
-            path = ep.get("path")
-            sourcetype = ep.get("sourcetype")
-            response = self.get_request(ew, base_url, path, auth_type, username, password, iname)
-            
-            event = Event(
-                sourcetype=sourcetype,
-                stanza=input_name,
-                data=response,
-                host=input_item.get("host")
-            )
-            ew.write_event(event)
+        if auth_type == 'basic':
+            try:
+                if password != self.MASK:
+                    self.encrypt_password(username, password, session_key)
+                    self.mask_password(session_key, username, base_url, auth_type, input_name)
 
+                
+                for ep in endpoints:
+                    path = ep.get("path")
+                    sourcetype = ep.get("sourcetype")
+
+                    response = self.get_request(ew, base_url, path, auth_type, username, iname, session_key)
+
+                    ew.log('INFO', 'Responde: ' + response)
+
+                    event = Event(
+                        sourcetype=sourcetype,
+                        stanza=input_name,
+                        data=response,
+                        host=input_item.get("host")
+                    )
+                    ew.write_event(event)
+            
+            except Exception as e:
+                ew.log('ERROR','There was an error when encrypting/masking the password: %s' %e)
+        
+        else:
+
+            for ep in endpoints:
+                path = ep.get("path")
+                sourcetype = ep.get("sourcetype")
+
+                response = self.get_request(ew, base_url, path, auth_type, username, iname, session_key)
+
+                event = Event(
+                    sourcetype=sourcetype,
+                    stanza=input_name,
+                    data=response,
+                    host=input_item.get("host")
+                )
+                ew.write_event(event)
 
 if __name__ == "__main__":
     sys.exit(NiFiScript().run(sys.argv))
